e1401a6ec14b36b13767b564f726366b8c7f499a
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-09-18 14:44:40 +0100

    feat: learnt about CRUD operation

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require("express");
 const morgan = require("morgan");
 //mongoose to connect to the database
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+
+const { render } = require("ejs");
+const blogRoutes = require("./routes/blogRoutes")
 
 //express app
 const app = express();
+
+
 
 //connect to mongodb
 const dbURL =
@@ -56,6 +60,8 @@ app.set("view engine", "ejs");
 
 //middleware for static files
 app.use(express.static("public"));
+//it takes the url data, and create an object of the data
+app.use(express.urlencoded({ extended: true }));
 //morgan middleware
 app.use(morgan("dev"));
 
@@ -75,70 +81,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//routes
-app.get("/", (req, res) => {
-  // res.send("Hello, World!")
-  // res.sendFile("./views/index.html", { root: __dirname });
-  const blogs = [
-    {
-      title: "heyyyy",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus necessitatibus, esse vero, unde quisquam deserunt consequatur hic molestiae fugit reiciendis porro minima et quas iusto sint a ut, nisi recusandae.",
-    },
-    {
-      title: "heyyyy",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus necessitatibus, esse vero, unde quisquam deserunt consequatur hic molestiae fugit reiciendis porro minima et quas iusto sint a ut, nisi recusandae.",
-    },
-    {
-      title: "heyyyy",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus necessitatibus, esse vero, unde quisquam deserunt consequatur hic molestiae fugit reiciendis porro minima et quas iusto sint a ut, nisi recusandae.",
-    },
-    {
-      title: "heyyyy",
-      snippet:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus necessitatibus, esse vero, unde quisquam deserunt consequatur hic molestiae fugit reiciendis porro minima et quas iusto sint a ut, nisi recusandae.",
-    },
-  ];
-  //render a view
-  // res.render("index", { title: "Home", blogs: blogs });
-  res.redirect("/blogs");
-});
 
-//routing and html
-app.get("/about", (req, res) => {
-  // res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about", { title: "About" });
-});
+//blog routes
 
-
-app.get("/blogs", (req, res) => {
-  //sorted from the newest to the oldest
-  Blog.find().sort({createdAt:-1})
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create-Blog" });
-});
-
-//404 page - this should go to the very bottom of the logic, because for every request made, it will check for the route, if there is no match it fires the 404 error page
-//this is an example of a middleware
-app.use((req, res) => {
-  // res.status(404).sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404", { title: "404" });
-});
-
-//middleware is a code that runs on a server, it is responsible for send getting a request and sending a response
-//e.g the app.use(func)
-//use cases for middleware
-//- it can be used to log details of every request
-//-it can be used for authentication checks for protected routes
-//- to parse JSON data from requests
-// to return 404 pages
+app.use(blogRoutes)
